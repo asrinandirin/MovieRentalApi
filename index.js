06af2b2fb3c0e4762527056ed0b20837eb7dda02
asrin andirin <asrnandrn@gmail.com>
2023-03-30 00:24:29 +0300

    jwt integrated for registering

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const homePageRoute = require('./routes/homePageRoute')
-const genreRoute = require('./routes/genreRoute')
-const movieRoute = require('./routes/movieRoute') 
-const customerRoute = require('./routes/customerRoute')
-const rentalRoute = require('./routes/rentalRoute')
-const userRoute = require('./routes/userRoute')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const homePageRoute = require('./routes/homePageRoute');
+const genreRoute = require('./routes/genreRoute');
+const movieRoute = require('./routes/movieRoute');
+const customerRoute = require('./routes/customerRoute');
+const rentalRoute = require('./routes/rentalRoute');
+const authRoute = require('./routes/authRoute');
+const userRoute = require('./routes/userRoute');
+const config = require('config');
+
+if (!config.get('jwtPrivateKey')) {
+  console.log('Fatal Error: JWT KEY NOT DEFINED');
+  process.exit(1);
+}
 
 // Connect MongoDB at default port 27017.
-mongoose.connect('mongodb://127.0.0.1:27017/MovieShop')
-.then(() => {
-    console.log('DB Connected')
+mongoose.connect('mongodb://127.0.0.1:27017/MovieShop').then(() => {
+  console.log('DB Connected');
 });
 
 app.use(express.json());
 
-app.use('/' , homePageRoute)
-app.use('/api/genres', genreRoute)
-app.use('/api/movies', movieRoute)
-app.use('/api/customers', customerRoute)
-app.use('/api/rentals', rentalRoute)
-app.use('/api/users', userRoute)
+app.use('/', homePageRoute);
+app.use('/api/genres', genreRoute);
+app.use('/api/movies', movieRoute);
+app.use('/api/customers', customerRoute);
+app.use('/api/rentals', rentalRoute);
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`App is listening in : ${PORT}`)
-})
+  console.log(`App is listening in : ${PORT}`);
+});
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const { User, validateUser } = require('../models/userModel');
+const { User } = require('../models/userModel');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const Joi = require('joi')
 const jwt = require('jsonwebtoken')
 const config = require('config')
 
@@ -17,17 +18,25 @@ router.post('/', async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send('User already registered');
+  if (!user) return res.status(400).send('Invalid email or password');
 
-  user = new User(_.pick(req.body, ['name', 'email', 'password']));
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-  await user.save();
+  const validPassword = await bcrypt.compare(req.body.password, user.password)
+
+  if(!validPassword) return res.status(400).send('Invalid email or password')
 
   const token = jwt.sign({_id: user._id, name: user.name}, config.get('jwtPrivateKey') )
 
-  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
-  
+  res.send(token)
 });
+
+const validateUser = (req) => {
+    const schema = Joi.object({
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    })
+
+    return schema.validate(req)
+
+}
 
 module.exports = router;
